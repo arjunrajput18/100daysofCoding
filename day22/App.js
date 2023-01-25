d75ab17ca7591b6de607fc9308281e7d733307d3
Arjunsingh Rajput <asr9320003120@gmail.com>
2023-01-25 18:20:38 +0530

    update

---
+++ day22/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "./component/Home"
+import About from './component/About';
+import NavBar from './component/NavBar';
+// import Router6 from './component/Router6';
+const App = () => {
+  return (
+    <div>
+   
+    <BrowserRouter>
+   <NavBar/>
+
+    <Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="/about" element={<About/>}/>
+
+</Routes>
+
+    </BrowserRouter>
+
+
+    </div>
+  );
+};
+
+export default App;
