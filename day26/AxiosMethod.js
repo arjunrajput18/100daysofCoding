23cc513e6110d3d37047170544dce8e469e4ad39
Arjunsingh Rajput <asr9320003120@gmail.com>
2023-01-28 20:31:25 +0530

    day 26

---
+++ day26/AxiosMethod.js
@@ -0,0 +1,39 @@
+//video 29
+//map function
+//react js is javascript libraryhere we only builds view i.e pages, here there is no concept like model and controller here we want to use api react js doesnt provide internally library. we want to import external library.
+//ek client hota hai ek server hota hai  ab react kya hai react kya samjta hai uske liye data kya hai ek tho props hoga and nhi tho state hoga . react ka syntax hee is taarh se design hai data aah raha yeah vho props may aayega  yea phir hum state may set karwadenge  yeah tho hum use empty kardenge react khali yeah samjta hai
+
+//always put axios componentDidMount() method. then it will work
+//data display for that used state
+
+import React, { Component } from "react";
+import axios from "axios";
+// import userEvent from "@testing-library/user-event";
+export default class AxiosMethod extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => this.setState({ posts: resp.data }));
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+
+        <h1>Hi i am Working</h1>
+      </div>
+    );
+  }
+}
